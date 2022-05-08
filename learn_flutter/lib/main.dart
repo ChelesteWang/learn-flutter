d79cb74db8164a6a860854924af309a1b80e52cc
ChelesteWang <1581446178@qq.com>
2022-05-08 20:40:45 +0800

    feat: 更新webview

--- learn_flutter/lib/main.dart
+++ learn_flutter/lib/main.dart
@@ -1,40 +1,96 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: RandomWords(),
-        ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(
+        key: Key('home'),
       ),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({required Key key}) : super(key: key);
 
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _RandomWordsState extends State<RandomWords> {
+class _MyHomePageState extends State<MyHomePage> {
+  late WebViewController _controller;
+
+  showToast(msg) {
+    Fluttertoast.showToast(
+        msg: msg,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 2,
+        backgroundColor: Colors.orange,
+        textColor: Colors.white,
+        fontSize: 16.0);
+  }
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    JavascriptChannel _alertJavascriptChannel(BuildContext context) {
+      return JavascriptChannel(
+          name: 'Toast',
+          onMessageReceived: (JavascriptMessage message) {
+            showToast(message.message);
+          });
+    }
+
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: const Text('WebView Test'),
+      // ),
+      // body: RaisedButton(
+      //   child: Text('点击按钮'),
+      //   onPressed: () {
+      //    showToast("hahah");
+      //   },
+      // )
+      body: WebView(
+        // initialUrl: 'http://www.baidu.com',
+        initialUrl: 'http://localhost:3000/',
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller = webViewController;
+        },
+        navigationDelegate: (NavigationRequest request) {
+          if (request.url.startsWith('js://webview')) {
+            // print('JS调用了 flutter by navigationDelegate');
+            showToast('调用了原生哦，😄😄');
+            // showToast('JS调用了 flutter by navigationDelegate:${request.url}');
+            return NavigationDecision.prevent; //阻止路由替換
+          }
+          // print('allowing navigation to $request');
+          return NavigationDecision.navigate; //允许路由替換
+        },
+        javascriptChannels: <JavascriptChannel>{
+          _alertJavascriptChannel(context),
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _controller
+              // ignore: deprecated_member_use
+              .evaluateJavascript('callJS("visible")')
+              .then((result) {
+            // You can handle JS result here.
+          });
+        },
+        child: const Text('原生调用Js'),
+      ),
+    );
   }
 }
